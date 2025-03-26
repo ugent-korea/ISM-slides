fc0bef2684fe81c698a7dc9161a967070b6ef7f1
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2025-03-27 07:25:50 +0900

    Create pkgs directory

--- install-requirements.R
+++ install-requirements.R
@@ -1,3 +1,8 @@
+# Create writable directory for R packages
+pkgs_dir <- Sys.getenv("R_LIBS_USER")
+dir.create(pkgs_dir, showWarnings = FALSE)
+
+# Install R packages
 packages <- c(
     "GGally",
     "MASS",

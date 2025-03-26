b8ee7c997a026249f2181bb20492a09530e78957
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2025-03-27 07:45:46 +0900

    Use R action

--- install-requirements.R
+++ install-requirements.R
@@ -1,9 +1,3 @@
-# Create writable directory for R packages
-pkgs_dir <- Sys.getenv("R_LIBS_USER")
-dir.create(pkgs_dir, showWarnings = FALSE)
-Sys.chmod(pkgs_dir, mode = "777", use_umask = TRUE)
-
-# Install R packages
 packages <- c(
     "GGally",
     "MASS",
